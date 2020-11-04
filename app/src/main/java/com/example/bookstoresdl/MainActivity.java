8f72938e244fff3e469b34f9e53230b4a5757e4b
prathmesh <prathmeshjagtap405@gmail.com>
2020-11-04 10:44:01 +0530

    added image and buttons

--- app/src/main/java/com/example/bookstoresdl/MainActivity.java
+++ app/src/main/java/com/example/bookstoresdl/MainActivity.java
@@ -1,14 +1,38 @@
+
 package com.example.bookstoresdl;
 
-import androidx.appcompat.app.AppCompatActivity;
+        import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+        import android.os.Bundle;
+        import android.widget.Button;
+
+        import com.example.bookstoresdl.R;
+
+        import java.security.PrivateKey;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button allbooks,novels,storybooks,studybooks,about;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initialize();
+    }
+
+    private void initialize() {
+        allbooks = findViewById(R.id.AllBooks);
+        novels = findViewById(R.id.Novels);
+        storybooks = findViewById(R.id.StoryBooks);
+        studybooks = findViewById(R.id.StudyBooks);
+        about = findViewById(R.id.About);
+
     }
 }
+
+
+
+
+
+
+
